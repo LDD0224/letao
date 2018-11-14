584e5f512dd92a4555dfbd2365ca00fa57132e26
ldd <2578011358@qq.com>
2018-11-14 15:08:52 +0800

    用户功能基本完成

---
+++ public/back1/js/user.js
@@ -0,0 +1,66 @@
+import { template } from "handlebars";
+
+$(function () {
+  
+  var currentPage = 1;
+  var pageSize = 5;
+  render();
+
+  function render() {
+    
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      success: function ( info ) {
+        console.log(info);
+        
+        var htmlStr = template("tpl", info);
+        $('.lt_content tbody').html(htmlStr);
+
+        $('#paginator').bootstrapPaginato({
+
+          bootstrapMajorVersion: 3,
+          currentPage: info.page,
+          totalPages: Math.ceil( info.total / info.size ),
+          onPageClicked: function ( a, b, c, page ) {
+            currentPage = page;
+            render();
+          }          
+        });
+      }
+    });
+  }
+
+
+  // 2. 通过事件委托给 按钮注册点击事件
+  $('.lt_content tbody').on("click", ".btn", function () {
+    
+    $('#userModal').modal("show");
+
+    var id = $(this).parent().data("id");
+    var isDelete = $(this).hasClass("btn-success") ? 1 : 0;
+
+    $('#subnitBtn').off("click").on("click", function () {
+      
+      $.ajax({
+        type: "post",
+        url: "/user/updateUser",
+        data: {
+          id: id,
+          isDelete: isDelete
+        },
+        success: function (info) {
+          if (info.success) {
+            $('#userModal').modal("hide");
+            render();
+          }
+        }
+      })
+    })
+  });
+  
+})
