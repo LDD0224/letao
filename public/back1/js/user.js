a22047267e572855d48be1a7b2d160e13fefd9c6
ldd <2578011358@qq.com>
2018-11-16 22:28:20 +0800

    用户界面静态完成

--- public/back1/js/user.js
+++ public/back1/js/user.js
@@ -1,16 +1,31 @@
 $(function () {
 
-  $.ajax({
-    type: "get",
-    url: "/user/queryUser",
-    data: {
-      page: 1,
-      pageSize: 5
-    },
-    dataType: "json",
-    success: function ( info ) {
-      var htmlStr = template("tmp", info);
-      $('tbody').html( htmlStr );
-    }
-  })
+  var currentPage = 1;
+  var pageSize = 5;
+  var currentId;
+  var isDelete;
+
+  render();
+ 
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      dataType: "json",
+      success: function ( info ) {
+        var htmlStr = template("tmp", info);
+        $('tbody').html( htmlStr );
+        // 进行分页初始化
+        $('#paginator').bootstarpPaginator({
+          bootstarpMajorVersion: 3,
+          totalPages: Math.ceil( info.total / info.size );
+        })        
+      }
+    })
+  };
+
 })
